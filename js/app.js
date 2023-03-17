836095e58ffafab892479f2d7b94f9a9a1fc204f
rjxwjfl <51114522+rjxwjfl@users.noreply.github.com>
2023-03-17 17:22:34 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    AsideEvent.getInstance().addEventShowMenuButton();
+    AsideEvent.getInstance().addEventMainChange();
+    InformationEvent.getInstance().addEventPhotoChangeClick();
+    InformationEvent.getInstance().addEventPhotoChange();
+    InformationEvent.getInstance().addEventAboutMeModifyClick();
+    InformationEvent.getInstance().addEventAboutMeSaveClick();
+    InformationEvent.getInstance().addEventIntroduceModifyClick();
+    InformationEvent.getInstance().addEventIntroduceSaveClick();
+    TodoEvent.getInstance().addEventAddTodoClick();
+    TodoEvent.getInstance().addEventAddTodoKeyUp();
+    TodoService.getInstance();
+}
